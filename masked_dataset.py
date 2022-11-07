054602bdb57c7f5aa579b19805748a826d828fce
Baris Sen <baris.sen@tum.de>
2022-11-07 00:56:22 -0800

    Fix and finalize dataset for CIFAR10

--- masked_dataset.py
+++ masked_dataset.py
@@ -14,9 +14,9 @@ class MaskedAutoencoderDataset(Dataset):
     def __init__(self, data, masking_ratio: float = 0.01):
         self.data = data
         self.masking_ratio = masking_ratio
-        self.input_length = data[0].shape[2]
+        self.input_length = data[0].shape[1]
         self.number_unmasked_indices = np.floor(
-            self.input_length * self.masking_ratio)
+            self.input_length * self.masking_ratio).astype(int)
 
     def __len__(self):
         return len(self.data)
@@ -25,41 +25,42 @@ class MaskedAutoencoderDataset(Dataset):
         full_input = self.data[idx]
         masked_input = torch.zeros_like(self.data[idx])
         unmasked_indices = np.random.permutation(self.input_length)
-
-        print("self.input_length", self.input_length)
-        print("unmasked_indices", unmasked_indices)
         unmasked_indices = unmasked_indices[:self.number_unmasked_indices]
-        print("unmasked_indices", unmasked_indices)
         masked_input[:, unmasked_indices] = full_input[:, unmasked_indices]
-        return masked_input, full_input
+        return masked_input, full_input, unmasked_indices
 
 
-def get_data(dataset: str = "CIFAR10"):
+def _prepare_CIFAR_input():
+    # Download / Load original data
+    train_val_set_original = CIFAR10(
+        root='data/', download=True, transform=ToTensor())
+    test_set_original = CIFAR10(
+        root='data/', train=False, transform=ToTensor())
+
+    # Split train/val sets
+    val_size = 500
+    train_size = len(train_val_set_original) - val_size
+    train_set_original, val_set_original = random_split(
+        train_val_set_original, [train_size, val_size])
+
+    # Ignore the classes and take only images
+    # We also flatten images into 1d
+    train_set_input = [train_set_original[i][0].flatten(
+        start_dim=1) for i in range(len(train_set_original))]
+    val_set_input = [val_set_original[i][0].flatten(
+        start_dim=1) for i in range(len(val_set_original))]
+    test_set_input = [test_set_original[i][0].flatten(
+        start_dim=1) for i in range(len(test_set_original))]
+    return train_set_input, val_set_input, test_set_input
+
+
+def get_data(dataset: str = "CIFAR10", masking_ratio: float = 0.01):
 
     if dataset not in SUPPORTED_DATASETS:
         raise RuntimeError(f"The dataset {dataset} is not supported.")
 
     if dataset == "CIFAR10":
-        # Download / Load original data
-        train_val_set_original = CIFAR10(
-            root='data/', download=True, transform=ToTensor())
-        test_set_original = CIFAR10(
-            root='data/', train=False, transform=ToTensor())
-
-        # Split train/val sets
-        val_size = 500
-        train_size = len(train_val_set_original) - val_size
-        train_set_original, val_set_original = random_split(
-            train_val_set_original, [train_size, val_size])
-
-        # Ignore the classes and take only images
-        train_set_input = [train_set_original[i][0].flatten(
-            start_dim=2) for i in range(len(train_set_original))]
-        val_set_input = [val_set_original[i][0].flatten(start_dim=2)
-                         for i in range(len(val_set_original))]
-        test_set_input = [test_set_original[i][0].flatten(
-            start_dim=2) for i in range(len(test_set_original))]
-
+        train_set_input, val_set_input, test_set_input = _prepare_CIFAR_input()
     elif dataset == "IMAGENET":
         # TODO: Implement the preprocessing for Imagenet
         raise NotImplementedError()
@@ -67,8 +68,8 @@ def get_data(dataset: str = "CIFAR10"):
         # TODO: Implement the preprocessing for Modelnet
         raise NotImplementedError()
 
-    train_ds = MaskedAutoencoderDataset(train_set_input)
-    val_ds = MaskedAutoencoderDataset(val_set_input)
-    test_ds = MaskedAutoencoderDataset(test_set_input)
+    train_ds = MaskedAutoencoderDataset(train_set_input, masking_ratio)
+    val_ds = MaskedAutoencoderDataset(val_set_input, masking_ratio)
+    test_ds = MaskedAutoencoderDataset(test_set_input, masking_ratio)
 
     return train_ds, val_ds, test_ds
